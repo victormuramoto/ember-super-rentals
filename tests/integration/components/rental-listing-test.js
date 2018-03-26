e7fe1158fe14683fd1c448379172d61284bb4506
Victor Muramoto <victor@muramoto@gmail.com>
2018-03-26 09:06:01 -0300

    adding files

---
+++ tests/integration/components/rental-listing-test.js
@@ -0,0 +1,37 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import EmberObject from '@ember/object';
+import { run } from '@ember/runloop';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | rental-listing', function(hooks) {
+  setupRenderingTest(hooks);
+
+  let rental = EmberObject.create({
+    image: 'fake.png',
+    title: 'test-title',
+    owner: 'test-owner',
+    category: 'test-type',
+    city: 'test-city',
+    bedrooms: 3
+  })
+  test('should display rental details', async function(assert) {
+    await this.set('rentalObj', rental);
+    await this.render(hbs`{{rental-listing rental=rentalObj}}`);
+
+    assert.equal(this.$('.listing h3').text(), 'test-title', 'Title: test-title');
+    assert.equal(this.$('.listing .owner').text().trim(), 'Owner: test-owner', 'Owner: test-owner');
+  });
+  
+  test('should toggle wide class on click', async function(assert) {
+    await this.set('rentalObj', rental);
+    await this.render(hbs`{{rental-listing rental=rentalObj}}`);
+
+    assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
+    run(() => document.querySelector('.image').click());
+    assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
+    run(() => document.querySelector('.image').click());
+    assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
+  });
+});
