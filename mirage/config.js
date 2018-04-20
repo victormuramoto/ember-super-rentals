ecda0943ae6dbb08f9b3f794f2c8b7c16da8a10e
Victor Muramoto <victor.muramoto@gmail.com>
2018-04-20 13:48:20 -0300

    Still working - missing tests - https://guides.emberjs.com/v3.0.0/tutorial/service/

--- mirage/config.js
+++ mirage/config.js
@@ -46,4 +46,4 @@ export default function() {
       return { data: rentals };
     }
   });
-};
+}
--- tests/unit/services/maps-test.js
+++ tests/unit/services/maps-test.js
@@ -1,5 +1,16 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
+
+const DUMMY_ELEMENT = {};
+
+let MapUtilStub = EmberObject.extend({
+  createMap(element, location) {
+    this.assert.ok(element, 'createMap called with element');
+    this.assert.ok(location, 'createMap called with location');
+    return DUMMY_ELEMENT;
+  }
+})
 
 module('Unit | Service | maps', function(hooks) {
   setupTest(hooks);
@@ -8,6 +19,15 @@ module('Unit | Service | maps', function(hooks) {
   test('it exists', function(assert) {
     let service = this.owner.lookup('service:maps');
     assert.ok(service);
+  });
+
+  test('should create a new map if one isnt cached for location', function (assert) {
+    assert.expect(4);
+    let stubMapUtil = MapUtilStub.create({ assert });
+    let mapService = { mapUtil: stubMapUtil };
+    let element = mapService.getMapElement('San Francisco');
+    assert.ok(element, 'element exists');
+    assert.equal(element.className, 'map', 'element has class name of map');
   });
 });
 
